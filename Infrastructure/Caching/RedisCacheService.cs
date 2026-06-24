92d79c130654c99270a5c822495fa01ef34f0064
hema24790@gmail.com <hema24790@gmail.com>
2026-06-24 19:32:54 +0300

    Prevent redis from breaking the application when not available

--- Infrastructure/Caching/RedisCacheService.cs
+++ Infrastructure/Caching/RedisCacheService.cs
@@ -1,35 +1,63 @@
-﻿using StackExchange.Redis;
+﻿using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 using System.Text.Json;
 
 namespace Infrastructure.Caching
 {
-    internal class RedisCacheService: ICache
+    internal class RedisCacheService : ICache
     {
         private readonly IDatabase _database;
-        public RedisCacheService(IConnectionMultiplexer connectionMultiplexer)
+        private readonly ILogger<RedisCacheService> _logger;
+        public RedisCacheService(IConnectionMultiplexer connectionMultiplexer, ILogger<RedisCacheService> logger)
         {
             _database = connectionMultiplexer.GetDatabase();
+            _logger = logger;
         }
 
         public async Task<TData> GetAsync<TData>(string key, CancellationToken cancellationToken = default)
         {
-            var value = await _database.StringGetAsync(key);
-            
-            if (value.IsNullOrEmpty)
-                return default!;
+            try
+            {
+                var value = await _database.StringGetAsync(key);
 
-            return JsonSerializer.Deserialize<TData>(value.ToString())!;
+                if (value.IsNullOrEmpty)
+                    return default!;
+
+                return JsonSerializer.Deserialize<TData>(value.ToString())!;
+            }
+            catch (RedisException ex)
+            {
+                _logger.LogWarning(ex, "Failed to retrieve cache entry with key '{CacheKey}'", key);
+
+                return default;
+            }
         }
 
         public async Task<bool> SetAsync<TData>(string key, TData data, TimeSpan? expiration = null, CancellationToken cancellationToken = default)
         {
-            var value = JsonSerializer.Serialize(data);
-            return await _database.StringSetAsync(key, value, expiration, When.Always);
+            try
+            {
+                var value = JsonSerializer.Serialize(data);
+                return await _database.StringSetAsync(key, value, expiration, When.Always);
+            }
+            catch (RedisException ex)
+            {
+                _logger.LogWarning(ex, "Failed to store cache entry with key '{CacheKey}'", key);
+
+                return false;
+            }
         }
 
         public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
         {
-            await _database.KeyDeleteAsync(key);
+            try
+            {
+                await _database.KeyDeleteAsync(key);
+            }
+            catch (RedisException ex)
+            { 
+                _logger.LogWarning(ex, "Failed to store cache entry with key '{CacheKey}'", key);
+            }
         }
     }
 }
